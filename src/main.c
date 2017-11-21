65330c506d5d3edcfb8e1d13886beb4caee5b1f2
Spectre <antoine@doussaud.org>
2017-11-21 18:06:20 +0100

    page allocate

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: adoussau <adoussau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/20 22:03:30 by adoussau          #+#    #+#             */
-/*   Updated: 2017/11/21 17:25:03 by adoussau         ###   ########.fr       */
+/*   Updated: 2017/11/21 17:55:10 by adoussau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,6 @@ t_block *getFreeBlock(t_page *p, size_t s) {
 		if (!ptr_block->state && (ptr_block->size > (s + sizeof(t_block)))) {
 			return ptr_block;
 		}
-		printf("Block used\n");
 		ptr_block = ptr_block->next;
 	}
 	return NULL;
@@ -67,6 +66,7 @@ t_block *malloc_test2(size_t s) {
 					ptr_page->blocks_nb++;
 					return (block+1);
 				}
+				else
 			ptr_page = ptr_page->next;
 		}
 	}
@@ -74,30 +74,30 @@ t_block *malloc_test2(size_t s) {
 }
 
 
-void	*malloc_test(size_t s) {
+void	*allocate_new_page(size_t s) {
 	if (pages == NULL) {
 		t_page *ptr;
 		printf("New Page\n");
 
 		ptr = (t_page*)mmap(
 								NULL,
-								page_size + sizeof(t_page),
+								s + sizeof(t_page),
 								PROT_READ | PROT_WRITE,
 								MAP_PRIVATE | MAP_ANON,
 								-1,
 								0
 							);
 		if (ptr) {
-			bzero(ptr,page_size+sizeof(t_page));
+			bzero(ptr, s + sizeof(t_page));
 			ptr->type = 42;
-			ptr->size = page_size;
+			ptr->size = s;
 			ptr->prev = NULL;
 			ptr->next = NULL;
 			ptr->data = NULL;
-			ptr->blocks_nb = 0xffffffff;
+			//ptr->blocks_nb = 0xffffffff;
 
 			t_block *block = ptr + 1;
-			block->size = page_size - sizeof(t_block);
+			block->size = s - sizeof(t_block);
 			block->prev = NULL;
 			block->next = NULL;
 			block->state = 0;
@@ -108,16 +108,21 @@ void	*malloc_test(size_t s) {
 			pages = ptr;
 			return (ptr->data + 1);
 		}
+		return NULL;
 	}
 }
 int		main(int argc, char **argv)
 {
 	printf("%d\n", getpagesize());
-	malloc_test(10);
+	allocate_new_page(500);
 	char *str1 = malloc_test2(10);
 	char *str2 = malloc_test2(10);
 	char *str3 = malloc_test2(10);
 	char *str4 = malloc_test2(10);
+
+	// char *str5 = malloc_test2(10);
+	//
+	// char *str6 = malloc_test2(10);
 
 	memcpy(str1, "0000000000", 10);
 	memcpy(str2, "1111111111", 10);
