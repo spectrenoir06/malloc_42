92ed9dccd15706a51b22e399f4e0fbb23d2de0b6
Antoine DOUSSAUD <adoussau@e2r5p15.42.fr>
2017-12-11 18:54:02 +0100

    norme

--- src/malloc.c
+++ src/malloc.c
@@ -103,11 +103,37 @@ void		free(void *ptr)
 	}
 }
 
+void	split_block_rea(t_block *ptr_block, size_t s)
+{
+	t_block *new_block;
+
+	new_block = (t_block *)(((char *)(ptr_block + 1)) + s);
+	new_block->size = ptr_block->next->size - (s - ptr_block->size);
+	new_block->next = ptr_block->next->next;
+	new_block->prev = ptr_block;
+	ptr_block->next = new_block;
+	ptr_block->size = s;
+	if (new_block->next)
+		new_block->next->prev = new_block;
+}
+
+void	*rea_cpy(t_block *block, size_t s)
+{
+	char *rea;
+
+	if ((rea = malloc(s)))
+	{
+		memcpy(rea, block + 1, block->size);
+		free(block);
+		return (rea);
+	}
+	return (NULL);
+}
+
 void		*realloc(void *ptr, size_t s)
 {
 	t_page	*ptr_page;
 	t_block	*ptr_block;
-	t_block	*new_block;
 
 	ptr_block = search_block(ptr, &ptr_page);
 	if (ptr_block)
@@ -117,27 +143,10 @@ void		*realloc(void *ptr, size_t s)
 		else
 		{
 			if (ptr_block->next && ptr_block->next->state == 0
-				&& (s <= (ptr_block->size + ptr_block->next->size)))
-			{
-				new_block = (t_block *)(((char *)(ptr_block + 1)) + s);
-				new_block->size = ptr_block->next->size - (s - ptr_block->size);
-				new_block->next = ptr_block->next->next;
-				new_block->prev = ptr_block;
-				ptr_block->next = new_block;
-				ptr_block->size = s;
-				if (new_block->next)
-					new_block->next->prev = new_block;
-			}
+			&& (s <= (ptr_block->size + ptr_block->next->size)))
+				split_block_rea(ptr_block, s);
 			else
-			{
-				new_block = malloc(s);
-				if (new_block)
-				{
-					memcpy(new_block, ptr_block + 1, ptr_block->size);
-					free(ptr);
-					return (new_block);
-				}
-			}
+				return (rea_cpy(ptr, s));
 		}
 	}
 	return (NULL);
